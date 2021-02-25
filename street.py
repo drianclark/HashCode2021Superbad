2e9deab430f94fea7525a0ddbe3322da4b040cf3
drianclark <dreiclark@gmail.com>
2021-02-25 19:06:09 +0000

    lights to is_green in street class

--- street.py
+++ street.py
@@ -1,9 +1,9 @@
 class Street:
-    def __init__(self, name, length, start, end, cars=[], lights=[]):
+    def __init__(self, name, length, start, end, cars=[], is_green=False):
         self.length = length
         self.start = start
         self.end = end
         self.name = name
         self.cars = cars
-        self.lights = lights
+        self.is_green = False
         
