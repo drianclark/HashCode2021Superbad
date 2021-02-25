4fbfa9f85a46750f66b2303c4078d53a38bdd808
drianclark <dreiclark@gmail.com>
2021-02-25 18:38:18 +0000

    beginnings of input parser

--- input_parser.py
+++ input_parser.py
@@ -1,17 +1,17 @@
-from . import Street
+# from . import Street
 
 class InputParser:
     def __init__(self, input_file):
-        self.in = input_file
+        self.input_file = input_file
     
     def getMetadata(self):
-        with open(self.in) as f:
+        with open(self.input_file) as f:
             metadata = f.readline().rstrip("\n").split(" ")
             
         return metadata
             
     def getStreets(self):
-        with open(self.in) as f:
+        with open(self.input_file) as f:
             streetLines = f.readlines()[1:]
             for line in streetLines:
                 lineContents = line.rstrip("\n").split(" ")
@@ -20,5 +20,28 @@ class InputParser:
                     x = int(lineContents[1]) 
                 except:
                     break
+                
+                start, end, name, length = lineContents
+                print(start, end, name, length)
+    
+    def getPaths(self):
+        with open(self.input_file) as f:
+            streetLines = f.readlines()[1:]
             
-                streetObject = Street(lineContents[0], lineContents[1], lineContents[2], lineContents[3])
+            for line in streetLines:
+                lineContents = line.rstrip("\n").split(" ")
+                                
+                if lineContents[1].isnumeric():
+                    continue
+                else:
+                    car_id = lineContents[0]
+                    car_path = lineContents[1:]
+                    
+                    print(car_id)
+                    print(car_path)
+                    print()
+
+        
+a = InputParser("a.txt")
+a.getStreets()
+a.getPaths()
