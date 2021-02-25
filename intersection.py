bc88cf8e6e40bcf40e5f534aac1ca9857040fb1c
david <devans-morga@github.com>
2021-02-25 19:14:55 +0000

    update street and intersection classes

--- intersection.py
+++ intersection.py
@@ -1,6 +1,45 @@
 class Intersection:
-    def __init__(self, on_light, incoming, outgoing):
+        def __init__(self, incoming, outgoing):
+        """
+        on_light : ID of street that is currently GREEN
+        incoming : dict of connecting (incoming) Street objects keyed by name 
+        outgoing : dict of outgoing Street objects keyed by name
+        """
+        self.incoming = {} 
+        self.outgoing = {}
 
-        self.on_light = on_light
-        self.incoming = incoming
-        self.outgoing = outgoing
+    def go(self, street):
+        """
+        Takes a street object as input, set this street's traffic light to GREEN
+        and all other lights connected to this intersection 
+        """
+        if street.name not in self.incoming:
+            if street.name in self.outgoing:
+                raise Exception(f"{street.name} is an outgoing street for this intersection")
+            else:
+                raise Exception(f"{street.name} is not connected to this intersection")
+        else:
+            # set all to red
+            for street in self.incoming:
+                street.light.setRed()
+            
+            # change only the light of the specified street to green
+            self.incoming[street.name].light.setGreen()
+
+
+    def addIncomingStreet(self, street):
+        if street.name in self.outgoing:
+            raise Exception(f"{street.name} is already outgoing from this intersection")
+        if street.name not in self.incoming:
+            self.incoming[street.name] = street
+        else:
+            print("street is already incoming to this intersection")
+
+
+    def addOutgoingStreet(self, street):
+        if street.name in self.incoming:
+            raise Exception(f"{street.name} is already incoming to this intersection")
+        if street.name not in self.outgoing:
+            self.incoming[street.name] = street
+        else:
+            print("street is already outgoing from this intersection")
--- street.py
+++ street.py
@@ -1,12 +1,33 @@
 class Street:
-    def __init__(self, name, length, start, end, cars=[], is_green=False):
-        self.length = length
+    def __init__(self, name, length, start, end, cars, lights):
+        self.light = TrafficLight()
+        self.length = length # == Time
         self.start = start
         self.end = end
         self.name = name
-        self.length = length
         self.cars = cars
-        self.is_green = False
-        
+
     def __repr__(self):
         return f"Street {self.name} - (Start: {self.start}, End: {self.end}, Length: {self.length})."
+
+class TrafficLight:
+
+    COLOURS = ['RED', 'GREEN']
+
+    def __init__(self):
+        # red by default
+        self.colour = 'RED'
+
+    def __bool__(self):
+        """ Return true if we can go! """
+        return self.colour == 'GREEN'
+
+    def setRed(self):
+        self.colour = 'RED'
+
+    def setGreen(self):
+        self.colour = 'GREEN'
+
+    def change(self):
+        """ change from one state to the other """
+        self.colour = 'GREEN' if self.colour == 'RED' else 'RED'
